1fc97fad7cb71d21db76ef0b521545902cf724d3
Vincent <vincentjiang918@gmail.com>
2026-09-12 21:44:53 -0400

    a

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_uwasic_onboarding_vincent (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -15,13 +15,30 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+    assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+    assign uio_out = 0;
+    // Add this inside the module block
+    assign uio_oe = 8'hFF; // Set all IOs to output
+    // Create wires to refer to the values of the registers
+    wire [7:0] en_reg_out_7_0;
+    wire [7:0] en_reg_out_15_8;
+    wire [7:0] en_reg_pwm_7_0;
+    wire [7:0] en_reg_pwm_15_8;
+    wire [7:0] pwm_duty_cycle;
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    // Instantiate the PWM module
+    pwm_peripheral pwm_peripheral_inst (
+        .clk(clk),
+        .rst_n(rst_n),
+        .en_reg_out_7_0(en_reg_out_7_0),
+        .en_reg_out_15_8(en_reg_out_15_8),
+        .en_reg_pwm_7_0(en_reg_pwm_7_0),
+        .en_reg_pwm_15_8(en_reg_pwm_15_8),
+        .pwm_duty_cycle(pwm_duty_cycle),
+        .out({uio_out, uo_out})
+    );
+    
+    // Add uio_in and ui_in[7:3] to the list of unused signals:
+    wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
 
 endmodule
